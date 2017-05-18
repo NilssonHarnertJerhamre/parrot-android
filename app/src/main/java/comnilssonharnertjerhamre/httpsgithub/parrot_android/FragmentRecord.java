5dbf25fcff7681cba341faf5b49870ddee366382
Jerhamre <anton.jerhamre@gmail.com>
2017-05-18 17:01:37 +0200

    upload recordings to server implemented

--- app/src/main/java/comnilssonharnertjerhamre/httpsgithub/parrot_android/FragmentRecord.java
+++ app/src/main/java/comnilssonharnertjerhamre/httpsgithub/parrot_android/FragmentRecord.java
@@ -19,12 +19,26 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-import static android.R.attr.button;
-import static android.R.attr.delay;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * Created by antonjerhamre on 2017-05-17.
@@ -35,14 +49,17 @@ public class FragmentRecord extends Fragment implements View.OnClickListener {
 
     ImageButton recordButton;
     Button playBackButton;
+    Button uploadButton;
 
     int delay = 10; //milliseconds
     Handler h = new Handler();
     private MediaRecorder mRecorder = null;
     private MediaPlayer mediaPlayer;
+    String file;
+
+    private RequestQueue queue;
 
     private boolean mStartRecording;
-    private boolean mStartPlaying;
 
 
 
@@ -65,6 +82,9 @@ public class FragmentRecord extends Fragment implements View.OnClickListener {
         recordButton.setOnClickListener(this);
         playBackButton = (Button) view.findViewById(R.id.playback_button);
         playBackButton.setOnClickListener(this);
+        uploadButton = (Button) view.findViewById(R.id.upload_button);
+        uploadButton.setOnClickListener(this);
+
         mStartRecording = true;
         return view;
     }
@@ -73,6 +93,7 @@ public class FragmentRecord extends Fragment implements View.OnClickListener {
     public void onStart(){
         super.onStart();
         mediaPlayer = new MediaPlayer();
+        queue = Volley.newRequestQueue(getActivity());
     }
 
     @Override
@@ -89,6 +110,9 @@ public class FragmentRecord extends Fragment implements View.OnClickListener {
                 break;
             case R.id.playback_button:
                 onPlayBack();
+                break;
+            case R.id.upload_button:
+                onUpload();
                 break;
         }
     }
@@ -183,4 +207,69 @@ public class FragmentRecord extends Fragment implements View.OnClickListener {
         mediaPlayer.stop();
         mediaPlayer.reset();
     }
+
+    public void onUpload(){
+        ByteArrayOutputStream buffer;
+        byte[] bytes;
+        String mFileName = getActivity().getExternalCacheDir().getAbsolutePath();
+        mFileName += "/audiorecordtest.3gp";
+        try {
+            file = getStringFromFile(mFileName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String url = "http://ec2-52-35-30-107.us-west-2.compute.amazonaws.com:45678/chirp";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", "felfelfelfel");
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("parrot", String.valueOf(DataHolder.getData()));
+                params.put("chirp", file);
+
+                return params;
+            }
+        };
+        queue.add(postRequest);
+    }
+
+    public static String convertStreamToString(InputStream is) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
+
+    public static String getStringFromFile (String filePath) throws Exception {
+        File fl = new File(filePath);
+        FileInputStream fin = new FileInputStream(fl);
+        String ret = convertStreamToString(fin);
+        //Make sure you close all streams.
+        fin.close();
+        return ret;
+    }
+
 }
